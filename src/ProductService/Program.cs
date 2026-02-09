14a99db61ee9693d040b55e49d5d4cbe1d34c384
dmlyczynski <damian.mlyczynski@gmail.com>
2026-02-09 15:15:44 +0100

    Update configuration and product service

--- src/ProductService/Program.cs
+++ src/ProductService/Program.cs
@@ -1,3 +1,88 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using ProductService;
+using Serilog;
 
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddOpenTelemetry(logging =>
+{
+    logging.IncludeFormattedMessage = true;
+    logging.IncludeScopes = true;
+});
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(Helpers.ConfigureResource)
+    .WithTracing(tracerProviderBuilder =>
+        tracerProviderBuilder
+            .AddSource(Instrumentor.ServiceName)
+            .AddAspNetCoreInstrumentation(opts =>
+            {
+                opts.Filter = context =>
+                {
+                    var ignore = new[] { "/swagger" };
+                    return !ignore.Any(s => context.Request.Path.ToString().Contains(s));
+                };
+            })
+            .AddHttpClientInstrumentation())
+    .WithMetrics(metricsProviderBuilder =>
+        metricsProviderBuilder
+            .AddMeter(Instrumentor.ServiceName)
+            .AddRuntimeInstrumentation()
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation())
+    .UseOtlpExporter();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var secretKey = jwtSettings["SecretKey"] ?? throw new InvalidOperationException("JWT SecretKey is not configured");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+    };
+});
+
+builder.Services.AddAuthorization();
+
+// ToDo
+builder.Services.AddHealthChecks();
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(8080);
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapHealthChecks("/health");
+
+app.MapControllers();
+
+app.Run();
